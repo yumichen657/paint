abfeae23c0ba07efe32914257c131c36354d23a8
yumichen657 <frank05231158@gmail.com>
2025-08-22 21:22:47 +0800

    Create level2.js

---
+++ levels/level2.js
@@ -0,0 +1,14 @@
+const levelConfig = {
+  boardData: [
+    ['red','red','red','red','red','red','red','red','red','red'],
+    ['red','green','green','green','red','red','green','green','green','red'],
+    ['red','green','blue','green','green','green','green','blue','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','red','red','red','red','red','red','red','red','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','red','red','red','red','red','red','red','red','red']
+  ],
+  target: 'yellow',
+  maxMoves: 3
+};
