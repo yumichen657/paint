0cb2ba6ff022c6b56d3cf9b55f6b907b6c4dd75a
yumichen657 <frank05231158@gmail.com>
2025-08-22 21:20:30 +0800

    Create level1.js

---
+++ levels/level1.js
@@ -0,0 +1,14 @@
+const levelConfig = {
+  boardData: [
+    ['red','red','red','red','red','red','red','red','red','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','green','green','green','green','green','green','green','green','red'],
+    ['red','red','red','red','red','red','red','red','red','red'],
+    ['red','blue','blue','blue','blue','blue','blue','blue','blue','red']
+  ],
+  target: 'blue',
+  maxMoves: 2
+};
